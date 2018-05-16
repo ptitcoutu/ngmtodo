d4806eaa560b427ed3eb4993c90ae3a5f9f18a2f
Vincent Couturier <vincent.couturier@whoz.com>
2018-05-16 17:24:46 +0200

    migrate to angular and add a first observable example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {BehaviorSubject, Observable} from 'rxjs';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  showDialog = false;
+  editingTodo: any = null;
+  fieldValue = '';
+  todoList: any[] = [];
+  $todoList$: BehaviorSubject<string[]> = new BehaviorSubject([]);
+  todoListLenght$: Observable<number> = this.$todoList$.map((todoList: any[]) => {
+    console.log(todoList);
+    return todoList.length;
+  });
+
+  okButtonText = 'Create task';
+
+  todoDialog(todo = null) {
+    this.okButtonText = 'Create task';
+    this.fieldValue = '';
+    this.editingTodo = todo;
+    if (todo) {
+      this.fieldValue = todo.title;
+      this.okButtonText = 'Edit task';
+    }
+    this.showDialog = true;
+  }
+
+  remove(index: number) {
+    this.todoList.splice(index, 1);
+    this.$todoList$.next(this.todoList);
+  }
+
+  editTodo(title) {
+    this.editingTodo.title = title;
+  }
+
+  updateTodo(title) {
+    if (title) {
+      title = title.trim();
+      if (this.editingTodo) {
+        this.editTodo(title);
+      } else {
+        this.addTodo(title);
+      }
+    }
+    this.hideDialog();
+  }
+
+  addTodo(title) {
+    const todo = {title: title, completed: false};
+    this.todoList.push(todo);
+    this.$todoList$.next(this.todoList);
+  }
+
+  hideDialog() {
+    this.showDialog = false;
+    this.editingTodo = null;
+    this.fieldValue = null; // make sure Input is new
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {
+  MatDialogModule, MatInputModule, MatFormFieldModule, MatCardModule, MatToolbarModule, MatButtonModule,
+  MatCheckboxModule, MatListModule, MatIconModule
+} from '@angular/material';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { DialogComponent } from './dialog/dialog.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatDialogModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule, MatToolbarModule,
+    MatButtonModule, MatCheckboxModule, MatListModule, MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
